fecf2cdb573e767fed9faf3317968e09fa64eb03
ismailtepeli <43654629+ismailtepeli@users.noreply.github.com>
2018-11-11 21:42:19 +0300

    ilk tasarım

---
+++ CafeApp/CafeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CafeApp.Startup))]
+namespace CafeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

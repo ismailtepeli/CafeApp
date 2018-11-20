5e9c3c1977844c14b3fda2ae37d1e2b91d8f3764
ismailtepeli <43654629+ismailtepeli@users.noreply.github.com>
2018-11-20 21:07:09 +0300

    dashboard

---
+++ CafeApp/CafeApp/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            ViewData["ActiveMenu"] = "Günlük İşlemler";
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            ViewData["ActiveMenu"] = "Menü";
+            return View();
+        }
+    }
+}
